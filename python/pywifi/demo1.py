4188db30087e6c1ec72a2adedbba997c64e2c81b
ChengToSpectre <111869107+ChengToSpectre@users.noreply.github.com>
2022-08-26 17:27:54 +0800

    Update demo1.py

--- python/pywifi/demo1.py
+++ python/pywifi/demo1.py
@@ -1,2 +1,30 @@
 import pywifi
-from pywifi import 
+from pywifi import const
+
+def gic():
+    
+    wifi = pywifi.PyWiFi()#创建一个无线对象
+
+    ifaces = wifi.interfaces()[0]#获得第一个无线网卡
+
+    wifiname = ifaces.name()#获得网卡的名字
+
+    wifistatus = ifaces.status()#获得WiFi状态
+
+    if wifistatus == const.IFACE_DISCONNECTED:#0没有可读性
+        print("not link to wifi")
+    else :
+        print("have linked to wifi")
+    
+    bies(ifaces,wifistatus)
+
+def bies(ifaces,wifistatus):
+
+    if wifistatus == const.IFACE_DISCONNECTED:
+        
+        ifaces.scan()#通过网卡扫描wifi
+
+        print(ifaces.scan_results)#查看扫描结果
+
+
+gic()
